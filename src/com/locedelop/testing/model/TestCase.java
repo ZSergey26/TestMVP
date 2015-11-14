6b7609a7a95ce9c66d8aa3989e9c555a3033287f
Sergey Zarochentsev <zsergey26@gmail.com>
2015-11-14 19:37:45 +0200

    Json

--- src/com/locedelop/testing/model/TestCase.java
+++ src/com/locedelop/testing/model/TestCase.java
@@ -1,5 +1,7 @@
 package com.locedelop.testing.model;
 
+import com.google.gson.Gson;
+
 /**
  * Created by Sergei Zarochentsev on 14.11.2015.
  */
@@ -26,6 +28,8 @@ public class TestCase {
         return new TestCase("_____ name is Robert", "Me", "I", "My", "He", 3);
     }
 
+
+
     @Override
     public String toString() {
         return "TestCase{" +
@@ -36,5 +40,37 @@ public class TestCase {
                 ", answer4='" + answer4 + '\'' +
                 ", correctAnswer=" + correctAnswer +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TestCase testCase = (TestCase) o;
+
+        if (correctAnswer != testCase.correctAnswer) return false;
+        if (question != null ? !question.equals(testCase.question) : testCase.question != null) return false;
+        if (answer1 != null ? !answer1.equals(testCase.answer1) : testCase.answer1 != null) return false;
+        if (answer2 != null ? !answer2.equals(testCase.answer2) : testCase.answer2 != null) return false;
+        if (answer3 != null ? !answer3.equals(testCase.answer3) : testCase.answer3 != null) return false;
+        return !(answer4 != null ? !answer4.equals(testCase.answer4) : testCase.answer4 != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = question != null ? question.hashCode() : 0;
+        result = 31 * result + (answer1 != null ? answer1.hashCode() : 0);
+        result = 31 * result + (answer2 != null ? answer2.hashCode() : 0);
+        result = 31 * result + (answer3 != null ? answer3.hashCode() : 0);
+        result = 31 * result + (answer4 != null ? answer4.hashCode() : 0);
+        result = 31 * result + correctAnswer;
+        return result;
+    }
+
+    public static TestCase createFromJson(String json) {
+        Gson gson = new Gson();
+        return gson.fromJson(json, TestCase.class);
     }
 }
--- src/test/TestingTest.java
+++ src/test/TestingTest.java
@@ -3,6 +3,8 @@ package test;
 import com.locedelop.testing.model.TestCase;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 /**
  * Created by Sergei Zarochentsev on 14.11.2015.
  */
@@ -10,6 +12,24 @@ public class TestingTest {
 
     @Test
     public void simpleTestCreation() {
-        org.junit.Assert.assertNotNull("should not be null", TestCase.createSimpleTestCase());
+        assertNotNull("should not be null", TestCase.createSimpleTestCase());
+    }
+
+    @Test
+    public void createFromJSON() {
+        TestCase expectedTestCase = TestCase.createSimpleTestCase();
+
+        String json = "{\n" +
+                "\t\"question\" : \"_____ name is Robert\",\n" +
+                "\t\"answer1\"  : \"Me\",\n" +
+                "\t\"answer2\"  : \"I\",\n" +
+                "\t\"answer3\"  : \"My\",\n" +
+                "\t\"answer4\"  : \"He\",\n" +
+                "\t\"correctAnswer\" : 3\n" +
+                "}";
+
+        TestCase jsonTestCase = TestCase.createFromJson(json);
+
+        assertEquals("Must be equals", expectedTestCase, jsonTestCase);
     }
 }
