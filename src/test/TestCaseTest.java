cccb5860bf4b0fffe5ed94baab47fe403274a14e
Sergey Zarochentsev <zsergey26@gmail.com>
2015-11-14 20:17:47 +0200

    Gradle

---
+++ src/test/TestCaseTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sergei Zarochentsev on 14.11.2015.
+ */
+public class TestCaseTest {
+
+    @Test
+    public void testEquals() throws Exception {
+
+    }
+}
